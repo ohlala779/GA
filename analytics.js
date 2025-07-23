6fa7ac485e50bf181acee832e887a414a710f7b7
ohlala779 <regdomainkris@gmail.com>
2025-07-22 21:07:57 -0700

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,16 @@
+(function () {
+  var analyticsCode = 'G-MY5YTPS5S2';
 
+  var scriptElement = document.createElement('script');
+  scriptElement.src = "https://www.googletagmanager.com/gtag/js?id=" + analyticsCode;
+  scriptElement.setAttribute("async", "async");
+  document.body.appendChild(scriptElement);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+  gtag('config', analyticsCode);
+})();
